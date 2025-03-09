09005bafda03d9593f6645d2a810fa20b6bd05c4
Aqil Rahman <43434399+aqiill@users.noreply.github.com>
2025-03-09 09:37:22 +0700

    Updates

---
+++ assets/js/5e95c892.e49696ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkeldes_docs=self.webpackChunkkeldes_docs||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var c=r(4164),d=r(1082),u=r(204),a=r(2831),l=r(1410),n=r(4848);function t(e){return(0,n.jsx)(d.e3,{className:(0,c.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(l.A,{children:(0,a.v)(e.route.routes)})})}}}]);
---
+++ assets/js/acecf23e.96f08c48.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkeldes_docs=self.webpackChunkkeldes_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
